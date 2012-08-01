a341a2538ef68cd4337e007d940e415c7c5ec5db
Steve Tipton <steve.tipton@employmentguide.com>
2012-08-01 14:42:32 -0400

    Add accepts_nested_attributes_for :weights and accessibility

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,9 @@
 class Question < ActiveRecord::Base
-  attr_accessible :content, :choices_attributes
+  attr_accessible :content, :choices_attributes, :weights_attributes
   has_many :choices, dependent: :destroy
   has_many :weights, dependent: :destroy
   has_many :surveys, through: :weights
   accepts_nested_attributes_for :choices, reject_if: lambda { |a| a[:content].blank? },
                                       allow_destroy: true
+  accepts_nested_attributes_for :weights
 end
