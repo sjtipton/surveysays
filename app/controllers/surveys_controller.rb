72d77d12ae4c8c2db23ced84f5fa2b07f9486b5f
Steve Tipton <steve.tipton@employmentguide.com>
2012-08-01 14:15:11 -0400

    Clean up surveys_controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,87 +1,48 @@
 class SurveysController < ApplicationController
-  # GET /surveys
-  # GET /surveys.json
   def index
     @surveys = Survey.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @surveys }
-    end
   end
 
-  # GET /surveys/1
-  # GET /surveys/1.json
   def show
     @survey = Survey.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @survey }
-    end
   end
 
-  # GET /surveys/new
-  # GET /surveys/new.json
   def new
     @survey = Survey.new
     @survey.questions << Question.new
     @survey.questions.each { |q| q.choices << Choice.new }
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @survey }
-    end
   end
 
-  # GET /surveys/1/edit
-  def edit
-    @survey = Survey.find(params[:id])
-  end
-
-  # POST /surveys
-  # POST /surveys.json
   def create
     @survey = Survey.new(params[:survey])
     @survey.questions << Question.new(params[:question])
     @survey.questions.each { |q| q.choices << Choice.new(params[:choice]) }
 
-    respond_to do |format|
-      if @survey.save
-        format.html { redirect_to @survey, notice: 'Survey was successfully created.' }
-        format.json { render json: @survey, status: :created, location: @survey }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @survey.errors, status: :unprocessable_entity }
-      end
+    if @survey.save
+      redirect_to @survey, notice: "Successfully created survey."
+    else
+      render :new
     end
   end
 
-  # PUT /surveys/1
-  # PUT /surveys/1.json
+  def edit
+    @survey = Survey.find(params[:id])
+  end
+
   def update
     @survey = Survey.find(params[:id])
 
-    respond_to do |format|
-      if @survey.update_attributes(params[:survey])
-        format.html { redirect_to @survey, notice: 'Survey was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @survey.errors, status: :unprocessable_entity }
-      end
+    if @survey.update_attributes(params[:survey])
+      redirect_to @survey, notice: "Successfully updated survey."
+    else
+      render :edit
     end
   end
 
-  # DELETE /surveys/1
-  # DELETE /surveys/1.json
   def destroy
     @survey = Survey.find(params[:id])
     @survey.destroy
 
-    respond_to do |format|
-      format.html { redirect_to surveys_url }
-      format.json { head :no_content }
-    end
+    redirect_to surveys_url, notice: "Successfully destroyed survey."
   end
 end
