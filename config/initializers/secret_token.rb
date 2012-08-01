1a02bb4ee1a9609c28650daa883e5c50b7e3c74a
Steve Tipton <steve.tipton@employmentguide.com>
2012-08-01 10:00:11 -0400

    initial commit for new rails app: surveysays

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveysays::Application.config.secret_token = '81e0df75fe63cd74d97edbfd71a1e21699af1097eac115bb7eadd234251a4f99fd6e9b15b080d958900e2291c72a6ea4a16c74362108063d2a15ca3454dc64e7'
