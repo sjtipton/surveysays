3bd7973089fb777090a0e508b9200e41357d27ba
Steve Tipton <steve.tipton@employmentguide.com>
2012-08-01 15:24:51 -0400

    Add weight as accessible attribute

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  attr_accessible :content, :choices_attributes
+  attr_accessible :content, :weight, :choices_attributes
   has_many :choices, dependent: :destroy
   belongs_to :survey
   accepts_nested_attributes_for :choices, reject_if: lambda { |a| a[:content].blank? },
