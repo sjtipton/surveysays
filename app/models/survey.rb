363a62400bea39a885bc0b11adf26abddcbd207f
Steve Tipton <steve.tipton@employmentguide.com>
2012-08-01 13:19:29 -0400

    Allow removal of questions

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,5 +2,5 @@ class Survey < ActiveRecord::Base
   attr_accessible :name, :questions_attributes
   has_many :weights, dependent: :destroy
   has_many :questions, through: :weights
-  accepts_nested_attributes_for :questions
+  accepts_nested_attributes_for :questions, allow_destroy: true
 end
