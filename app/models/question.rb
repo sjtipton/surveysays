9e4a17c078a5b9772166e1b93c6e2667b6f93f8d
Steve Tipton <steve.tipton@employmentguide.com>
2012-08-01 13:20:09 -0400

    Add accepts_nested_attributes_for :choices and accessibility

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
-  attr_accessible :content
+  attr_accessible :content, :choices_attributes
   has_many :choices, dependent: :destroy
   has_many :weights, dependent: :destroy
   has_many :surveys, through: :weights
+  accepts_nested_attributes_for :choices, allow_destroy: true
 end
