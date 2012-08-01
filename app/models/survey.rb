293ae64700b3faec2cd8a3f22ce524aff3be0045
Steve Tipton <steve.tipton@employmentguide.com>
2012-08-01 13:11:31 -0400

    Add accepts_nested_attributes_for :questions and accessibility

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :questions_attributes
   has_many :weights, dependent: :destroy
   has_many :questions, through: :weights
+  accepts_nested_attributes_for :questions
 end
