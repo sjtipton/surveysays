ec87fdec032cb4e38b7f3e703e732e86b8216eb9
Steve Tipton <steve.tipton@employmentguide.com>
2012-08-01 11:14:57 -0400

    Remove survey_id from questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120801142006) do
+ActiveRecord::Schema.define(:version => 20120801151304) do
 
   create_table "choices", :force => true do |t|
     t.string   "content"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(:version => 20120801142006) do
 
   create_table "questions", :force => true do |t|
     t.string   "content"
-    t.integer  "survey_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
